094522dbf458adfb063a4617c02fe45c4886a918
ArtemTur <tur_ne@mail.ru>
2023-04-18 00:21:51 +0300

    feat: add repeater 18 апр 2023 г.  0:21:51

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let object = new Object(options);
+ let string = str + object.addition + object.additionSeparator;
+ let string2 = string.repeat.repeatTimes.split.object.separator;
+ let string3 = string2.repeat.additionRepeatTimes;
+ return string3
 }
 
 module.exports = {
