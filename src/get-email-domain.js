a135fdc84cf3da7b637d8ddb1f2af4c65ef62d05
ArtemTur <tur_ne@mail.ru>
2023-04-18 01:02:00 +0300

    feat: add mail 18 апр 2023 г.  1:02:00

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let address;
+  let arr = email.split();
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] === "@"){
+     address = arr.slice(i+1);
+    }
+    let res = address.join('');
+    return res
+  }
+
+  
 }
 
 module.exports = {
